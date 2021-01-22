f1e17004d3f3cd00e461d2d01fad87a06e6729e8
Tcbirney <30777251+Tcbirney@users.noreply.github.com>
2021-01-22 11:46:48 -0500

    using commented out version of model. Trying to get each module working in pc execution thread

--- functions/AddGF2m.m
+++ functions/AddGF2m.m
@@ -11,6 +11,9 @@ elseif in2 == inf
 elseif in1 == in2
     sum = Inf; %changed
 else
+    % had to put this sum = 0 here bacsue simulink whines about sum not
+    % begin initialized in some decision trees
+    sum = 0;
     gfIndx = in1 + 2; %account for inf and indexing
     r_in1 = gf_matrix(gfIndx, :);
 
