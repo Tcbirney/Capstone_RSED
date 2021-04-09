70e771c94984a1620fe5e1165f9e8b4fad25a346
Tcbirney <30777251+Tcbirney@users.noreply.github.com>
2021-04-09 16:12:20 -0400

    removed display from divgf2. Test working

--- functions/DivGF2m.m
+++ functions/DivGF2m.m
@@ -2,10 +2,6 @@
 %in1, in2, product in power notation MSB on the left
 function [quotient] = DivGF2m(dividend, divisor, gf_matrix)
 m = numel(gf_matrix(1,:));
-if (divisor == -1)
-    msg = 'ERROR: divisor cannot be INF';
-    error(msg)
-end
 
 if(dividend == -1)
     quotient = -1;
