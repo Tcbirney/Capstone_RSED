d0f6352da463c38f03495a212856f361d552ebaa
dthoma4 <dthoma4@umbc.edu>
2021-05-15 00:06:10 -0400

    Please Work

--- functions/work/jetty-0.0.0.0-32415-lais_server_impl.jar-_-any-/webapp/remote/proxy/1.1/CheckJsFile.js
+++
@@ -1,8 +0,0 @@
-/*
- Test file for use with iFrameProxyClient.js
- iFrameProxyClient.js tries to load this file in a script tag.
- If the file fails to load with an error, we short circuit
- the wait to load "iFrameProxyRelease.html" and assume
- it will fail. Removing this file will cause iFrameProxyClient
- to think the iframe will fail to load.
- */
