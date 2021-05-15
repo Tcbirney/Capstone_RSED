d6758d9d416bb63606f514f58ffbccfb2d308b8b
dthoma4 <dthoma4@umbc.edu>
2021-05-15 00:09:53 -0400

    Please Work 2

---
+++ functions/EncSystematicm.m
@@ -0,0 +1,46 @@
+function code_word = EncSystematicm(poly,gen,gf_matrix)
+m = numel(gf_matrix(1,:)); % set m to number of rows (power of 2)
+numShifts = int8((2^m) - 1);
+power = numShifts - m; % find power which is n-k
+
+% Initialize x array full of 0s
+x = zeros(1,int8(power+1));
+
+% Filling everything but the first index with INF (-1)
+for i = (power+1):-1:2
+x(i) = -1;
+end
+%
+% % Shifting the temp polynomial
+temp_cw = PolyMultGF2m(x,poly,gf_matrix);
+
+% temp_cw/gen = remainder
+% get the remainder
+[q,r] = PolyDivGF2m(temp_cw,gen,gf_matrix);
+
+% if the remainder array is smaller than the codeword array
+
+% find the offset between the codeword - remainder array
+temp_r = -1*ones(1,numShifts); % 1x7 array
+
+if(numel(temp_cw) > numel(r))
+    diff = numel(temp_cw) - numel(r);
+    
+    counter = 1;
+
+    for i = 1:numShifts
+        temp_r(i) = r(counter);
+        if(i>diff)
+            counter = counter + 1;
+        end
+    end
+end
+
+% Initialize codeword full of 0s
+code_word = zeros(1, numShifts);
+
+% Create the actual codeword using addition
+% iterate through each index backwards
+for i = numShifts:-1:1
+code_word(i) = AddGF2m(temp_cw(i),temp_r(i),gf_matrix);
+end
---
+++ functions/chienSearch.m
@@ -0,0 +1,37 @@
+function [errorPos, numRoots] = chienSearch(lambda, degLambda, gf_matrix)
+%msg = ['Executing Chien Search...'];
+%disp(msg);
+rootIndx = 1;
+hasRoots = 0;
+root_len = numel(lambda)-2;
+roots = ones(1, root_len);
+errorPos = ones(1, numel(roots));
+
+    for i = 0: numel(roots)
+        eval = EvalPolyGF2m(lambda, i, gf_matrix);
+        if eval == -1
+            roots(1, rootIndx) = i;
+            rootIndx = rootIndx + 1;
+            hasRoots = 1;
+        end
+    end
+    numRoots = rootIndx - 1;
+    if hasRoots == 1
+        for i1 = 1:numel(roots)
+            errorPos(i1) = DivGF2m(0, roots(i1), gf_matrix);
+        end
+        if numRoots < degLambda
+            errorPos = -2;
+            %fail = ['DECODER FAILURE. THE NUMBER OF ROOTS IS LESS THAN THE DEGREE OF LAMBDA'];
+            %disp(fail);
+        end
+        %rootPrint = ['roots found: [', num2str(roots(:).'),']'];
+        %disp(rootPrint);
+        %print = ['error locations: [' ,num2str(errorPos(:).'), ']'];
+        %disp(print);
+    else
+        errorPos = -2;
+        %print = 'DECODER FAILURE: No roots found. LAMBDA is irreducible over the Galois Field';
+        %disp(print);
+    end
+end
